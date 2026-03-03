97ec4864b3db08bcc6b7b170a4c5af28b2dee50a
alexdozer <alex.dozer@gmail.com>
2026-03-03 14:16:06 +0100

    initial commit

---
+++ tests/Domain/SmokeTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Domain;
+
+use Tests\TestCase;
+
+class SmokeTest extends TestCase
+{
+    public function test_phpunit_and_autoload_work(): void
+    {
+        $this->assertTrue(true);
+    }
+
+}
